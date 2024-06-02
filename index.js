74184458a76faa6b521240616f0a8dd9358301be
Emisael Kisler <emisael@croney.io>
2024-06-02 11:45:39 -0400

    Completando ejercicios hasta el 3.18

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const { connectDB } = require("./mongo");
-
-const { Person } = require("./mongo.js");
+const { Person } = require("./src/models/persons.js");
+const errorHandler = require("./src/middleware/errorHandler.js");
 
 const app = express();
 app.use(cors());
@@ -27,31 +27,51 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/persons", async (request, response) => {
-  const { name, number } = request.body;
+// Ruta POST para crear un nuevo registro
+app.post("/api/persons", async (request, response, next) => {
+  try {
+    const { name, number } = request.body;
 
-  if (!name || !number) {
-    return response.status(400).json({
-      error: "Missing name or number",
-    });
+    if (!name || !number) {
+      return response.status(400).json({
+        error: "Missing name or number",
+      });
+    }
+
+    // Verifica si el nombre ya existe en la agenda
+    const existingPerson = await Person.findOne({ name });
+    if (existingPerson) {
+      return response.status(400).json({
+        error: "name must be unique",
+      });
+    }
+
+    const person = new Person({ name, number });
+    await person.save();
+
+    response.json(person);
+  } catch (error) {
+    next(error);
   }
-
-  // Verifica si el nombre ya existe en la agenda
-  const existingPerson = await Person.findOne({ name });
-  if (existingPerson) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  // Genera un ID aleatorio utilizando Math.random()
-
-  const person = new Person({ name, number });
-  await person.save();
-
-  response.json(person);
 });
 
+// Ruta PUT para actualizar todos los campos de un registro existente
+app.put("/api/persons/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatePerson) => {
+      response.json(updatePerson);
+    })
+    .catch((error) => next(error));
+});
+
+// Ruta GET para obtener todos los registros
 app.get("/", (request, response) => {
   response.send("<h1>Hallo Welt!</h1>");
 });
@@ -66,7 +86,8 @@ app.get("/api/persons", async (request, response) => {
   }
 });
 
-app.get("/api/persons/:id", async (request, response) => {
+// Ruta GET ID para obtener un registro por id
+app.get("/api/persons/:id", async (request, response, next) => {
   const id = request.params.id;
   try {
     const person = await Person.findById(id);
@@ -76,26 +97,39 @@ app.get("/api/persons/:id", async (request, response) => {
       response.status(404).send("Not found");
     }
   } catch (error) {
+    next(error);
+  }
+});
+
+// Ruta DELETE para eliminar un registro
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// Ruta GET para obtener la información del servidor
+app.get("/info", async (request, response) => {
+  try {
+    const date = new Date();
+
+    const persons = await Person.countDocuments();
+    console.log("persons", persons);
+    response.send(`<p>Phonebook has info for ${persons} people</p>
+    <p>${date}</p>`);
+  } catch (error) {
     console.error(error);
     response.status(500).send("Internal Server Error");
   }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-});
-
-app.get("/info", (request, response) => {
-  const date = new Date();
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>`);
-});
-
 app.use(unknownEndpoint);
 
+app.use(errorHandler);
+
+// Puerto de la aplicación
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
